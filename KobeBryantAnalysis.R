3f3c3d68aa9a7855ce68fdf33fa52424bf0b2b15
Michael Okuda <michael.okuda99@gmail.com>
2023-11-22 12:33:55 -0700

    rf

--- KobeBryantAnalysis.R
+++ KobeBryantAnalysis.R
@@ -6,35 +6,33 @@ library(discrim)
 library(naivebayes)
 library(kknn)
 library(kernlab)
+library(lightgbm)
 library(themis)
+library(bonsai)
 
 data <- vroom("./STAT 348/FinalProject/data.csv")
 data$shot_made_flag <- factor(data$shot_made_flag)
-data_clean <- data[1:500, ] %>%
-  select(-c(combined_shot_type, team_id, team_name, matchup, season, playoffs, game_id, period, lat, lon, shot_zone_range, game_date))
-data_clean_NA <- data_clean %>%
+data$time_remaining <- 48 - data$period + data$minutes_remaining
+
+data_train <- data[c("action_type", "shot_type", "shot_zone_area", "playoffs", "period", "time_remaining", "shot_made_flag")]
+
+data_train_final <- data_train %>%
   filter(!is.na(shot_made_flag))
+  
+data_test <- data %>%
+  filter(is.na(shot_made_flag)) %>%
+  select(c("action_type", "shot_type", "shot_zone_area", "playoffs", "period", "time_remaining", "shot_made_flag", "shot_id"))
 
-data_test <- data_clean_NA %>%
-  filter(is.na(shot_made_flag))
+all_levels <- unique(c(data_train$action_type, data_test$action_type))
+data_train_final$action_type <- factor(data_train_final$action_type, levels = all_levels)
+data_test$action_type <- factor(data_test$action_type, levels = all_levels)
 
-data_clean_final <- data_clean_NA %>%
-  select(-c(shot_id))
-
-view(cor(data_clean_final[, sapply(data_clean_final, is.numeric)]))
-table(data$game_event_id)
-table(data$lat)
-view(data)
-view(data_clean)
-
-recipe <- recipe(shot_made_flag ~ ., data=data_clean_final) %>%
-  step_mutate_at(game_event_id, fn=factor) %>%
-  step_lencode_mixed(all_nominal_predictors(), outcome=vars(shot_made_flag)) %>%
+recipe <- recipe(shot_made_flag ~ ., data=data_train_final) %>%
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>%
   step_dummy(all_nominal_predictors())
 
 prep <- prep(recipe)
-bake <- bake(prep, new_data=data_clean_final)
-# view(cor(bake[, sapply(bake, is.numeric)]))
+bake <- bake(prep, new_data=data_train_final)
 
 rf_model <- rand_forest(mtry = tune(),
                         min_n = tune(),
@@ -42,29 +40,22 @@ rf_model <- rand_forest(mtry = tune(),
   set_engine("ranger") %>%
   set_mode("classification")
 
-# Create a workflow with model & recipe
-# target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
-#   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
-#   step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
-# prep <- prep(target_encoding_amazon_recipe)
-# baked_train <- bake(prep, new_data = amazon_train)
-
 kobe_wf <- workflow() %>%
   add_recipe(recipe) %>%
   add_model(rf_model)
 
 # Set up grid of tuning values
-tuning_grid <- grid_regular(mtry(range=c(1,(ncol(data_clean_final) - 1))),
+tuning_grid <- grid_regular(mtry(range=c(1,(ncol(data_train_final) - 1))),
                             min_n(),
-                            levels = 3) ## L^2 total tuning possibilities
+                            levels = 5) ## L^2 total tuning possibilities
 
 # Set up K-fold CV
-folds <- vfold_cv(data_clean_final, v = 3, repeats=1)
+folds <- vfold_cv(data_train_final, v = 5, repeats=1)
 
 CV_results <- kobe_wf %>%
   tune_grid(resamples=folds,
             grid=tuning_grid,
-            metrics=metric_set(roc_auc)) #Or leave metrics NULL
+            metrics=metric_set(roc_auc))
 
 # Find best tuning parameters
 bestTune <- CV_results %>%
@@ -73,14 +64,58 @@ bestTune <- CV_results %>%
 # Finalize workflow and predict
 final_wf <- kobe_wf %>%
   finalize_workflow(bestTune) %>%
-  fit(data=data_clean_NA)
+  fit(data=data_train_final)
 
 predictions <- final_wf %>%
   predict(new_data = data_test, type="prob")
 
-predictions$shot_made_flag <- predictions$.pred_0
+predictions$shot_made_flag <- predictions$.pred_1
 predictions$shot_id <- data_test$shot_id
 kobe_final <- predictions %>%
   select(c(shot_id, shot_made_flag))
 
 write.csv(kobe_final, "./STAT\ 348/FinalProject/rfclassification.csv", row.names = F)
+
+## xgboost
+boost_model <- boost_tree(tree_depth=tune(),
+                          trees=tune(),
+                          learn_rate=tune()) %>%
+  set_engine("lightgbm") %>% #or "xgboost" but lightgbm is faster
+  set_mode("classification")
+
+boost_wf <- workflow() %>%
+  add_recipe(recipe) %>%
+  add_model(boost_model)
+
+# Fit or Tune Model HERE
+tuning_grid2 <- grid_regular(tree_depth(),
+                             trees(),
+                             learn_rate(),
+                             levels = 5) ## L^2 total tuning possibilities
+
+folds2 <- vfold_cv(data_train_final, v = 5, repeats=1)
+
+CV_results2 <- boost_wf %>%
+  tune_grid(resamples=folds2,
+            grid=tuning_grid2,
+            metrics=metric_set(roc_auc))
+
+# Predict
+bestTune2 <- CV_results2 %>%
+  select_best("roc_auc")
+
+# Finalize workflow and predict
+final_wf2 <- boost_wf %>%
+  finalize_workflow(bestTune2) %>%
+  fit(data=data_train_final)
+
+predictions2 <- final_wf2 %>%
+  predict(new_data = data_test, type="prob")
+
+predictions2$shot_made_flag <- predictions2$.pred_1
+predictions2$shot_id <- data_test$shot_id
+xgboost_final <- predictions2 %>%
+  select(c(shot_id, shot_made_flag))
+
+write.csv(xgboost_final, "./STAT\ 348/FinalProject/xgboostclassification.csv", row.names = F)
+
--- KobeBryantAnalysisBecker.R
+++ KobeBryantAnalysisBecker.R
@@ -6,57 +6,56 @@ library(discrim)
 library(naivebayes)
 library(kknn)
 library(kernlab)
+library(lightgbm)
 library(themis)
+library(bonsai)
 
 data <- vroom("./data.csv")
 data$shot_made_flag <- factor(data$shot_made_flag)
-data_clean <- data %>%
-  select(-c(combined_shot_type, team_id, team_name, matchup, season, playoffs, game_id, period, lat, lon, shot_zone_range, game_date))
-data_clean_NA <- data_clean %>%
+data$time_remaining <- 48 - data$period + data$minutes_remaining
+
+data_train <- data[c("action_type", "shot_type", "shot_zone_area", "playoffs", "period", "time_remaining", "shot_made_flag")]
+
+data_train_final <- data_train %>%
   filter(!is.na(shot_made_flag))
 
-data_test <- data_clean_NA %>%
-  filter(is.na(shot_made_flag))
+data_test <- data %>%
+  filter(is.na(shot_made_flag)) %>%
+  select(c("action_type", "shot_type", "shot_zone_area", "playoffs", "period", "time_remaining", "shot_made_flag", "shot_id"))
 
-data_clean_final <- data_clean_NA %>%
-  select(-c(shot_id))
+all_levels <- unique(c(data_train$action_type, data_test$action_type))
+data_train_final$action_type <- factor(data_train_final$action_type, levels = all_levels)
+data_test$action_type <- factor(data_test$action_type, levels = all_levels)
 
-recipe <- recipe(shot_made_flag ~ ., data=data_clean_NA) %>%
-  step_mutate_at(all_nominal_predictors(), fn=factor) %>%
-  step_lencode_mixed(all_nominal_predictors(), outcome=vars(shot_made_flag))
+recipe <- recipe(shot_made_flag ~ ., data=data_train_final) %>%
+  step_mutate_at(all_numeric_predictors(), fn=factor) %>%
+  step_dummy(all_nominal_predictors())
 
 prep <- prep(recipe)
-bake <- bake(prep, new_data=data_clean_NA)
+bake <- bake(prep, new_data=data_train_final)
 
 rf_model <- rand_forest(mtry = tune(),
                         min_n = tune(),
-                        trees=800) %>%
+                        trees=500) %>%
   set_engine("ranger") %>%
   set_mode("classification")
-
-# Create a workflow with model & recipe
-# target_encoding_amazon_recipe <- recipe(ACTION~., data=amazon_train) %>%
-#   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
-#   step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
-# prep <- prep(target_encoding_amazon_recipe)
-# baked_train <- bake(prep, new_data = amazon_train)
 
 kobe_wf <- workflow() %>%
   add_recipe(recipe) %>%
   add_model(rf_model)
 
 # Set up grid of tuning values
-tuning_grid <- grid_regular(mtry(range=c(1,(ncol(data_clean_NA) - 1))),
+tuning_grid <- grid_regular(mtry(range=c(1,(ncol(data_train_final) - 1))),
                             min_n(),
                             levels = 5) ## L^2 total tuning possibilities
 
 # Set up K-fold CV
-folds <- vfold_cv(data_clean_NA, v = 5, repeats=1)
+folds <- vfold_cv(data_train_final, v = 5, repeats=1)
 
 CV_results <- kobe_wf %>%
   tune_grid(resamples=folds,
             grid=tuning_grid,
-            metrics=metric_set(roc_auc)) #Or leave metrics NULL
+            metrics=metric_set(roc_auc))
 
 # Find best tuning parameters
 bestTune <- CV_results %>%
@@ -65,15 +64,14 @@ bestTune <- CV_results %>%
 # Finalize workflow and predict
 final_wf <- kobe_wf %>%
   finalize_workflow(bestTune) %>%
-  fit(data=data_clean_NA)
+  fit(data=data_train_final)
 
 predictions <- final_wf %>%
   predict(new_data = data_test, type="prob")
 
-predictions$shot_made_flag <- predictions$.pred_0
+predictions$shot_made_flag <- predictions$.pred_1
 predictions$shot_id <- data_test$shot_id
 kobe_final <- predictions %>%
   select(c(shot_id, shot_made_flag))
 
 write.csv(kobe_final, "./rfclassification.csv", row.names = F)
-
